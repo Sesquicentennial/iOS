59cd88203c567ddcf5aad9456686ee400d239a11
chetaldrich <chetaldrich@gmail.com>
2016-03-02 14:42:05 -0600

    Handle malformed quests in the data service.
    On the occasion that there is a quest that is malformed that comes from
    the server, the app will not keep it.

--- Carleton150/QuestDataService.swift
+++ Carleton150/QuestDataService.swift
@@ -26,7 +26,6 @@ final class QuestDataService {
     class func requestQuest(theme: String = "", limit: Int = 5, completion:
         (success: Bool, result: [Quest]?) -> Void) {
             
-           
         let parameters = [
             "theme": theme,
             "limit": limit
@@ -42,68 +41,38 @@ final class QuestDataService {
 				
 				if let answer = json["content"].array {
 					for i in 0 ..< answer.count {
-						
-						// create empty array of waypoints
-						var wayPoints: [WayPoint] = []
-						
-						let points = answer[i]["waypoints"]
-						
-						// iterate through the waypoints from the server
-						for i in 0 ..< points.count {
-							let location = CLLocationCoordinate2D(
-								latitude: points[i]["lat"].double!,
-								longitude: points[i]["lng"].double!
-							)
-							
-							var clue = [String: AnyObject]()
-							var hint = [String: AnyObject]()
-							var completion = [String: AnyObject]()
-							
-							clue["text"] = points[i]["clue"]["text"].string!
-							hint["text"] = points[i]["hint"]["text"].string!
-							
-							if let compText = points[i]["completion"]["text"].string {
-								completion["text"] = compText
-							}
-							
-							
-							if let clueImage = points[i]["clue"]["image"]["image"].string {
-								clue["image"] = clueImage
-							}
-							if let hintImage = points[i]["hint"]["image"]["image"].string {
-								hint["image"] = hintImage
-							}
-							if let compImage = points[i]["completion"]["image"].string {
-								completion["image"] = compImage
-							}
-							
-							wayPoints.append(
-								WayPoint(location: location,
-									radius: points[i]["rad"].double!,
-									clue: clue,
-									hint: hint,
-									completion: completion
-								)
-							)
-						}
-						
-						quests.append(
-							Quest(
-								wayPoints: wayPoints,
-								name: answer[i]["name"].string!,
-								description: answer[i]["desc"].string!,
-								completionMessage: answer[i]["compMsg"].string!,
-								creator: answer[i]["creator"].string!,
-								image: answer[i]["image"].string!,
-								difficulty: answer[i]["difficulty"].int!,
-								audience: answer[i]["audience"].string!
-							)
-						)
-						
+                        if let name = answer[i]["name"].string,
+                            description = answer[i]["desc"].string,
+							completionMessage = answer[i]["compMsg"].string,
+							creator = answer[i]["creator"].string,
+							image = answer[i]["image"].string,
+							difficulty = answer[i]["difficulty"].int,
+                            audience = answer[i]["audience"].string {
+                            
+                            let points = answer[i]["waypoints"]
+                            
+                            quests.append(
+                                Quest(
+                                    wayPoints: getWayPoints(points),
+                                    name: name,
+                                    description: description,
+                                    completionMessage: completionMessage,
+                                    creator: creator,
+                                    image: image,
+                                    difficulty: difficulty,
+                                    audience: audience
+                                )
+                            )
+                        } else {
+                           continue
+                        }
 					}
-					print(quests)
-					completion(success: true, result: quests)
-					
+                    if quests.count > 0 {
+                        completion(success: true, result: quests)
+                    } else {
+                        print("Results were malformed at endpoint: \(Endpoints.quests).")
+                        completion(success: false, result: nil)
+                    }
 				} else {
 					print("No results were found.")
 					completion(success: false, result: nil)
@@ -114,4 +83,49 @@ final class QuestDataService {
 			}
 		}
 	}
+    
+    
+    private class func getWayPoints(points: JSON) -> [WayPoint] {
+        // create empty array of waypoints
+        var wayPoints: [WayPoint] = []
+        
+        // iterate through the waypoints from the server
+        for i in 0 ..< points.count {
+            let location = CLLocationCoordinate2D(
+                latitude: points[i]["lat"].double!,
+                longitude: points[i]["lng"].double!
+            )
+            
+            var clue = [String: AnyObject]()
+            var hint = [String: AnyObject]()
+            var completion = [String: AnyObject]()
+            
+            clue["text"] = points[i]["clue"]["text"].string!
+            hint["text"] = points[i]["hint"]["text"].string!
+            
+            if let compText = points[i]["completion"]["text"].string {
+                completion["text"] = compText
+            }
+            if let clueImage = points[i]["clue"]["image"]["image"].string {
+                clue["image"] = clueImage
+            }
+            if let hintImage = points[i]["hint"]["image"]["image"].string {
+                hint["image"] = hintImage
+            }
+            if let compImage = points[i]["completion"]["image"].string {
+                completion["image"] = compImage
+            }
+            
+            wayPoints.append(
+                WayPoint(location: location,
+                    radius: points[i]["rad"].double!,
+                    clue: clue,
+                    hint: hint,
+                    completion: completion
+                )
+            )
+        }
+        
+        return wayPoints
+    }
 }
